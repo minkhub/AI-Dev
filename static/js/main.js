280a71470a4a7dfe3caa857d9e53b8392470a512
minkhub <130288626+minkhub@users.noreply.github.com>
2024-02-26 19:41:15 +0900

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,54 +1,35 @@
 $(document).ready(function () {
     // Init
-    $('.image-section').hide();
     $('.loader').hide();
     $('#result').hide();
 
-    // Upload Preview
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-                $('#imagePreview').hide();
-                $('#imagePreview').fadeIn(650);
-            }
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    $("#imageUpload").change(function () {
-        $('.image-section').show();
-        $('#btn-predict').show();
-        $('#result').text('');
-        $('#result').hide();
-        readURL(this);
-    });
-
     // Predict
     $('#btn-predict').click(function () {
-        var form_data = new FormData($('#upload-file')[0]);
+        var url_input = $('#url-input').val();
 
         // Show loading animation
         $(this).hide();
         $('.loader').show();
 
-        // Make prediction by calling api /predict
+        // Make prediction by calling API /predict
         $.ajax({
             type: 'POST',
             url: '/predict',
-            data: form_data,
-            contentType: false,
-            cache: false,
-            processData: false,
-            async: true,
+            data: { url: url_input },
             success: function (data) {
                 // Get and display the result
                 $('.loader').hide();
                 $('#result').fadeIn(600);
-                $('#result').text(' Result:  ' + data);
+                $('#result').text('This website is ' + data.website_status + '.\n' + 'The malicious probability is ' + data.malicious_probability.toFixed(2) + '%.');
                 console.log('Success!');
             },
+            error: function (xhr, status, error) {
+                // Handle errors
+                $('.loader').hide();
+                $('#result').text('Error: ' + error);
+                $('#result').fadeIn(600);
+                console.log('Error:', status, xhr.responseText);
+            }
         });
     });
-
 });
